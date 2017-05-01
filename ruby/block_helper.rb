a1c07bbb93e71baebee7e30af166c241b42b081a
Will Sheehan <wsheehan@bates.edu>
2017-05-01 13:32:27 -0400

    Add block method & comments on methods

--- ruby/block_helper.rb
+++ ruby/block_helper.rb
@@ -1,14 +1,17 @@
 require_relative 'block'
 
 module BlockHelper
+  # Blockchains require hard coded genesis blocks
   def genesis_block
     Block.new(0, "0", Time.now.to_i.to_s, "Genesis Block", Digest::SHA256.hexdigest('genesis block'))
   end
 
+  # New block require latest block info to proceed
   def latest_block(blockchain)
     blockchain[blockchain.length - 1]
   end
 
+  # Generate params for new block
   def generate_next_block(block_data)
     previous_block = latest_block
     block_index = previous_block.block_index + 1
@@ -17,6 +20,7 @@ module BlockHelper
     Block.new(block_index, previous_block.block_hash, timestamp, block_data, block_hash)
   end
 
+  # Ensure new block is indexed and hashed correctly
   def valid_new_block?(new_block, previous_block)
     conditions = {
       (new_block.block_index == (previous_block.block_index + 1)) => "Invalid index",
@@ -29,6 +33,12 @@ module BlockHelper
     end
   end
 
+  # Push new block into blockchain
+  def add_block(new_block, blockchain)
+    blockchain.push(new_block) if valid_new_block?(new_block, latest_block)
+  end
+
+  # Hash block params with SHA-256
   def calculate_block_hash(block)
     Digest::SHA256.hexdigest(block.block_index.to_s, block.previous_block_hash, block.timestamp, block.data)
   end
